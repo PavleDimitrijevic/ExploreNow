9395082d618458dd14825d19a8767a7ce5c17be8
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-01-21 19:17:34 +0100

    Created and connected monogodb database

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-import app from './app';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
 
-const port = 3001;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// Start the server
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
